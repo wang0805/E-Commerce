7901f977d319f44c82f657b867712e7b85418ec9
wang0805 <wh_wang@live.com>
2025-05-12 10:50:02 +0800

    10-category-pages

---
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// The use hook can be used in Server Components to read the value of a Promise and suspend rendering until the data is available. By passing a Promise, the framework enables the component (or a parent Suspense boundary) to handle the loading state gracefully. While the Promise is pending, a loading fallback can be shown. Once the Promise resolves, the component can render with the final params value. This can also simplify the code compared to managing the Promise with useEffect and state in older patterns.
+
+interface Props {
+  params: Promise<{ category: string; subcategory: string }>;
+}
+
+const Page = async ({ params }: Props) => {
+  // tjhis is a server way to get the params, client way is const params = useParams()
+  const { category, subcategory } = await params;
+  return (
+    <div>
+      Category: {category} <br /> Subcategory: {subcategory}
+    </div>
+  );
+};
+
+export default Page;
